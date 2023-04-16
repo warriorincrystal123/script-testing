2fd02a2f63ae511b0729d778a3a5e409a51f6e5a
warriorincrystal123 <130286520+warriorincrystal123@users.noreply.github.com>
2023-04-16 22:18:32 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,2 @@
+let url = "https://api.telegram.org/bot6247038805:AAH0hDPQEUINvmXXAcRzu_kxprvXfdR4EH8/sendMessage?chat_id=1001968534412&text="
+radio.sendString("" + url + "The microbit is now up and running")
