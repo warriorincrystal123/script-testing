72e504617fb3d8f92d2bbbc283096d4a80257ff3
warriorincrystal123 <130286520+warriorincrystal123@users.noreply.github.com>
2023-04-19 21:38:53 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,2 @@
 let url = "https://api.telegram.org/bot6247038805:AAH0hDPQEUINvmXXAcRzu_kxprvXfdR4EH8/sendMessage?chat_id=1001968534412&text="
-radio.sendString("" + url + "The microbit is now up and running")
+radio.sendString("" + url + "microbit online")
